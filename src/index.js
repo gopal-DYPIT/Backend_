dbe77f85d481c621ff66e755a903f9aa9f495769
[Gopal Kadam] <[gopalkadam212@gmail.com]>
2024-04-08 12:10:48 +0530

    Error handing and server hosting done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+import { app } from "./app.js";
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -9,3 +11,11 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at PORT: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!! ", err);
+})
