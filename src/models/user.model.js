96156791f9611c17fcf1a75be33ad963b7c88916
[Gopal Kadam] <[gopalkadam212@gmail.com]>
2024-04-10 09:54:34 +0530

    Upload Setup Done

---
+++ src/models/user.model.js
@@ -0,0 +1,52 @@
+import Mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    coverImage: {
+      type: String,
+      require: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = Mongoose.model("User", userSchema);
