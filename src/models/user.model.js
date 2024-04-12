e7da5969a9b4e743ee55b8d1e62ddb9e761a4ce7
[Gopal Kadam] <[gopalkadam212@gmail.com]>
2024-04-12 14:07:59 +0530

    token generation code

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,4 +49,39 @@ const userSchema = new Schema(
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+    
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+      {
+          _id: this._id,
+          
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+          expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      }
+  )
+}
+
 export const User = Mongoose.model("User", userSchema);
